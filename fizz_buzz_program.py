bee9f4576f316699c5b54682c3451e1686efe8c8
chaitanyakeerthi <ckpsai8787@gmail.com>
2021-05-07 17:15:20 -0400

    created fizzz buzz program

---
+++ fizz_buzz_program.py
@@ -0,0 +1,19 @@
+i=1
+while i<101:
+    if i%3==0:
+        print("fizz")
+        i=i+1
+        continue
+    if i%5==0:
+        print("Buzz")
+        i=i+1
+        continue
+    else:
+        print(i)
+        i=i+1
+
+       
+    
+    
+    
+   
